c3e7045d832e65964fa5e0f37d1da021a9609625
kylin <liux1118@qq.com>
2021-12-19 22:56:33 +0800

    动态加载路由

--- src/app/index.js
+++ src/app/index.js
@@ -1,23 +1,26 @@
 const Koa = require('koa');
 // 导入bodyParser中间件，解析json
 const bodyParser = require('koa-bodyparser')
-// 导入router
-const userRouter = require('../router/user.router');
-const authRouter = require('../router/auth.router');
-
 // 导入错误信息处置方法
 const errorHandler = require('./error-handle');
+// 导入路由 升级版 动态加载路由
+const useRoutes = require('../router');
+
+// 导入router
+// const userRouter = require('../router/user.router');
+// const authRouter = require('../router/auth.router');
 
 const app = new Koa();
 
 app.use(bodyParser());
 
-app.use(userRouter.routes());
-// 判断某个请求方式有没有，如果没有，会返回不允许，或者返回当前这个，服务器是不支持的
-app.use(userRouter.allowedMethods());
+useRoutes(app);
+// app.use(userRouter.routes());
+// // 判断某个请求方式有没有，如果没有，会返回不允许，或者返回当前这个，服务器是不支持的
+// app.use(userRouter.allowedMethods());
 
-app.use(authRouter.routes());
-app.use(authRouter.allowedMethods());
+// app.use(authRouter.routes());
+// app.use(authRouter.allowedMethods());
 
 app.on('error', errorHandler);
 
--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -3,7 +3,7 @@ const Router = require('koa-router');
 
 const authRouter = new Router();
 
-// 根据authRouter.post('/login', login) 中需要的写
+// 根据authRouter.post('/login', verifyLogin, login) 中需要的写
 const {
   login
 } = require('../controller/auth.controller');
@@ -12,6 +12,6 @@ const {
   verifyLogin
 } = require('../middleware/auth.middleware');
 
-authRouter.post('/login',verifyLogin, login);
+authRouter.post('/login', verifyLogin, login);
 
 module.exports = authRouter;
